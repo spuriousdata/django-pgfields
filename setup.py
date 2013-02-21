6a0ea7c1bb5bad56ce3261be97045b8c16b6ab69
Jannis Leidel <jannis@leidel.info>
2013-02-21 18:04:01 +0100

    Bumped version up a notch.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-pgfields',
-    version='0.0.1',
+    version='0.0.2',
     author="Mike O'Malley",
     author_email='mikeo@outriggermedia.com',
     description='Postgres-specific db fields for django',
